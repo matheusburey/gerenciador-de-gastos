e57f70907cdca21c31f8e26705b8d3351d37bcd2
matheusburey <matheusbureidev@gmail.com>
2025-09-10 16:11:14 -0300

    feat: integrate with api Signup page

--- app/pages/auth/signup.vue
+++ app/pages/auth/signup.vue
@@ -38,44 +38,42 @@
           <form class="space-y-6" @submit.prevent="handleSignUp">
             <div class="space-y-4">
               <InputUi
-                v-model="name"
+                v-model="form.name"
                 label="Nome"
                 type="text"
                 placeholder="Seu nome"
-                required
                 autocomplete="given-name"
+                :error="errors.name"
               />
               <InputUi
-                v-model="email"
+                v-model="form.email"
                 label="Email"
-                type="email"
                 placeholder="seu@email.com"
-                required
                 autocomplete="email"
+                :error="errors.email"
               />
               <InputUi
-                v-model="password"
+                v-model="form.password"
                 label="Senha"
                 type="password"
                 placeholder="Crie uma senha"
-                required
                 autocomplete="new-password"
+                :error="errors.password"
               />
               
               <InputUi
-                v-model="confirmPassword"
+                v-model="form.confirmPassword"
                 label="Confirme a senha"
                 type="password"
                 placeholder="Digite a senha novamente"
-                required
                 autocomplete="new-password"
-                :error="passwordError"
+                :error="errors.confirmPassword"
               />
               
               <CheckboxUi
-                v-model="acceptedTerms" 
-                required
+                v-model="form.acceptedTerms" 
                 class="mt-4"
+                :error="errors.acceptedTerms"
               >
                 Eu concordo com os <a href="#" class="text-indigo-600 hover:text-indigo-500">Termos de Serviço</a> e <a href="#" class="text-indigo-600 hover:text-indigo-500">Política de Privacidade</a>
               </CheckboxUi>
@@ -104,39 +102,86 @@
   </div>
 </template>
 
-<script setup>
-import { ref, computed } from 'vue'
+<script lang="ts" setup>
+import { ref } from 'vue'
 import { useRouter } from 'vue-router'
+import { useAuthStore } from '~/stores/auth'
 
-const name = ref('')
-const email = ref('')
-const password = ref('')
-const confirmPassword = ref('')
-const acceptedTerms = ref(false)
-const isLoading = ref(false)
+const toast = useToast()
+const authStore = useAuthStore()
 const router = useRouter()
 
-const passwordError = computed(() => {
-  if (!password.value || !confirmPassword.value) return ''
-  return password.value !== confirmPassword.value ? 'As senhas não conferem' : ''
+const isLoading = ref(false)
+const form = ref({
+  name: '',
+  email: '',
+  password: '',
+  confirmPassword: '',
+  acceptedTerms: false
 })
+const errors = ref({
+  name: '',
+  email: '',
+  password: '',
+  confirmPassword: '',
+  acceptedTerms: ''
+})
+
+function validateForm() {
+  let isValid = true
+  const values = form.value
+  errors.value = {
+    name: '',
+    email: '',
+    password: '',
+    confirmPassword: '',
+    acceptedTerms: ''
+  }
+
+  if (values.name.length < 5) {
+    errors.value.name = 'Nome deve ter pelo menos 5 caracteres'
+    isValid = false
+  }
+  if (!values.email.includes('@')) {
+    errors.value.email = 'Email inválido'
+    isValid = false
+  }
+  if (values.password.length < 8) {
+    errors.value.password = 'Senha deve ter pelo menos 8 caracteres'
+    isValid = false
+  }
+  if (!values.confirmPassword) {
+    errors.value.confirmPassword = 'Confirme a senha'
+    isValid = false
+  }
+  if (values.password !== values.confirmPassword) {
+    errors.value.confirmPassword = 'As senhas não conferem'
+    isValid = false
+  }
+  if (!values.acceptedTerms) {
+    errors.value.acceptedTerms = 'Você deve aceitar os termos de serviço'
+    isValid = false
+  }
+  return isValid
+}
 
 const handleSignUp = async () => {
   try {
+    if (!validateForm()) return
     isLoading.value = true
-    
-    if (password.value !== confirmPassword.value) {
-      return // Error is shown by the passwordError computed property
+
+    const result = await authStore.signup(
+      form.value.name,
+      form.value.email,
+      form.value.password
+    )
+    if (result.success) {
+      toast.success({ title: 'Conta criada com sucesso!'})
+      await router.push('/dashboard')
     }
-    
-    // TODO: Implement signup logic
-    console.log('Signing up with:', { 
-      name: name.value,
-      email: email.value,
-      acceptedTerms: acceptedTerms.value
-    })
-    
-    await router.push('/dashboard')
+    else {
+      toast.error({ title: result.error })
+    }
   } catch (error) {
     console.error('Error signing up:', error)
   } finally {
