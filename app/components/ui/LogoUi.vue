163c00b3b8d1ec48f97032ee0c7d1a82e8792d4d
matheusburey <matheusbureidev@gmail.com>
2025-09-11 12:03:42 -0300

    feat: create logo component

---
+++ app/components/ui/LogoUi.vue
@@ -0,0 +1,12 @@
+<template>
+  <div class="flex items-center">
+    <IconLogo class="text-[3.8rem] pt-2 text-blue-600" />
+    <span
+      class="text-xl font-bold bg-gradient-to-r from-blue-600 to-indigo-600 bg-clip-text text-transparent"
+      >MoneyFlow</span
+    >
+  </div>
+</template>
+<script lang="ts" setup>
+import IconLogo from "~/assets/icons/logo.svg";
+</script>
