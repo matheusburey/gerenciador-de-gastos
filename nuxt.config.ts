defbaf45e65d73447cfae6e9386619fad9e26cb1
matheusburey <matheusbureidev@gmail.com>
2025-09-11 12:02:59 -0300

    feat: change version tailwindcss and add new libs

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,17 +1,25 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
+import tailwindcss from "@tailwindcss/vite";
+
 export default defineNuxtConfig({
-  compatibilityDate: '2025-07-15',
+  compatibilityDate: "2025-07-15",
   devtools: { enabled: true },
-  modules: ['@nuxtjs/tailwindcss', '@prisma/nuxt', '@pinia/nuxt', 'nuxt-toast'],
+  modules: ["@prisma/nuxt", "@pinia/nuxt", "nuxt-toast", "nuxt-svgo"],
+  css: ['~/assets/css/main.css'],
   components: [
     {
-      path: '~/components',
+      path: "~/components",
       pathPrefix: false,
     },
   ],
   prisma: {
     datasourceUrl: process.env.DATABASE_URL,
     autoSetupPrisma: true,
-    errorFormat: 'colorless',
+    errorFormat: "colorless",
   },
-})
+  vite: { plugins: [tailwindcss()] },
+  svgo: {
+    autoImportPath: '~/assets/icons/',
+  },
+
+});
