c87beab62899fb0499005b591ed94c954e43c37b
matheusburey <matheusbureidev@gmail.com>
2025-09-11 23:05:59 -0300

    fix: change prisma version

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -4,22 +4,16 @@ import tailwindcss from "@tailwindcss/vite";
 export default defineNuxtConfig({
   compatibilityDate: "2025-07-15",
   devtools: { enabled: true },
-  modules: ["@prisma/nuxt", "@pinia/nuxt", "nuxt-toast", "nuxt-svgo"],
-  css: ['~/assets/css/main.css'],
+  modules: ["@pinia/nuxt", "nuxt-toast", "nuxt-svgo"],
+  css: ["~/assets/css/main.css"],
   components: [
     {
       path: "~/components",
       pathPrefix: false,
     },
   ],
-  prisma: {
-    datasourceUrl: process.env.DATABASE_URL,
-    autoSetupPrisma: true,
-    errorFormat: "colorless",
-  },
   vite: { plugins: [tailwindcss()] },
   svgo: {
-    autoImportPath: '~/assets/icons/',
+    autoImportPath: "~/assets/icons/",
   },
-
 });
